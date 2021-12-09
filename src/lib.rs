91377e2324f8856e772d83939c70d7d7cba2cf67
Ryan-Symplectic <ryan@symcsx.com>
2021-12-08 18:08:18 -0600

    Cleaned up code, updated messages

--- src/lib.rs
+++ src/lib.rs
@@ -23,62 +23,55 @@ use anyhow::Result;
 
 type Tx = UnboundedSender<Message>;
 type Am<T> = Arc<Mutex<T>>;
-pub type PubSubState = Am<HashMap<SocketAddr, Tx>>;
+pub type ServerState = Am<HashMap<SocketAddr, Tx>>;
 
 /**
  * !This thread is always looping as fast as possible, this is inefficient
  * No longer! With the new changes this is almost costless in terms of compute.
  * Costs around 308kb of ram tho
  */
-async fn data_intermediary<T: Serialize>(rx: Receiver<T>, subs: PubSubState) {
-    println!("[SUCCESS]: DATA PIPELINE CONSTRUCTED");
+async fn intermediary<T: Serialize>(rx: Receiver<T>, subs: ServerState, server_name: &str) {
+    println!("[SUCCESS]: {}: DATA PIPELINE CONSTRUCTED", server_name);
 
     for x in rx.iter() {
         if let Ok(s) = serde_json::to_string(&x) {
-            //println!("New message outgoing: {}", &s);
-            // we can probably parallelize this part some more
-
             let msg = Message::Text(s);
-            //println!("[INTERMEDIARY]: Calling Ballista");
             tokio::spawn(ballista(subs.clone(), msg));
-            //println!();
         }
     }
 
-    // we need to kill the channel
+    // we need to kill the channel?
 }
 
-async fn ballista(recipients: PubSubState, message: Message) {
-    let tmp = recipients.lock().unwrap();
-    //println!("[BALLISTA]: Sending message to {} subs", tmp.len());
-    for (_, sock) in tmp.iter() {
-        //println!("SENDING over unbounded sender");
-        sock.unbounded_send(message.clone()).unwrap();
+async fn ballista(recipients: ServerState, message: Message) {
+    let tmp = recipients.lock().unwrap(); // got a poison error on this thing when killing 200 conns at once
+
+    for (_, sock) in tmp.iter() { // might be able to parallelize further using Rayon. might not be a good idea tho
+        sock.unbounded_send(message.clone());//.unwrap(); // unused result
     }
 }
 
-async fn listener<K: ToSocketAddrs>(bind_addr: K, state: PubSubState) -> Result<()> {
+async fn listener<K: ToSocketAddrs>(bind_addr: K, state: ServerState, server_name: &'static str) -> Result<()> {
     let tcp_socket = TcpListener::bind(bind_addr).await?;
     println!(
-        "[SUCCESS]: LISTENER BOUND TO {}",
+        "[SUCCESS]: {}: LISTENER BOUND TO {}",
+        server_name,
         tcp_socket.local_addr().unwrap()
     );
 
     while let Ok((stream, addr)) = tcp_socket.accept().await {
         println!("Spawning new conn!");
-        tokio::spawn(json_server_conn_handler(state.clone(), stream, addr));
+        tokio::spawn(json_server_conn_handler(state.clone(), stream, addr, server_name));
     }
 
     println!("listener death");
     Ok(())
 }
 
-async fn json_server_conn_handler(state: PubSubState, raw_stream: TcpStream, addr: SocketAddr) {
-    //println!("Incoming TCP connection from: {}", addr);
-
+async fn json_server_conn_handler(state: ServerState, raw_stream: TcpStream, addr: SocketAddr, server_name: &str) {
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
-        .expect("Error during the websocket handshake occurred");
+        .expect(&format!("[ERROR]: {}: error accepting connection from {}", server_name, addr));
 
     let (tx, rx) = unbounded();
     state.lock().unwrap().insert(addr, tx);
@@ -86,7 +79,7 @@ async fn json_server_conn_handler(state: PubSubState, raw_stream: TcpStream, add
     let (outgoing, _) = ws_stream.split();
 
     let receiver = rx.map(Ok).forward(outgoing);
-    receiver.await;
+    receiver.await;// unused result
 
     state.lock().unwrap().remove(&addr);
 }
@@ -94,8 +87,10 @@ async fn json_server_conn_handler(state: PubSubState, raw_stream: TcpStream, add
 pub async fn launch_server<T: Serialize + Send + 'static, U: ToSocketAddrs + Send + 'static>(
     source: Receiver<T>,
     addr: U,
-) {
-    let state: PubSubState = Arc::new(Mutex::new(HashMap::new()));
-    task::spawn(listener(addr, state.clone()));
-    task::spawn(data_intermediary(source, state.clone()));
+    name: &'static str
+) -> ServerState {
+    let state: ServerState = Arc::new(Mutex::new(HashMap::new()));
+    task::spawn(listener(addr, state.clone(), name));
+    task::spawn(intermediary(source, state.clone(), name));
+    state
 }
